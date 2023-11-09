7fd56a3f06d7142d87d8fb6f15b9ba3e65d19e0a
Azim Chowdhury <azimchy994@gmail.com>
2023-11-09 13:35:51 +0600

    concurrent

--- hooks.test.js
+++ hooks.test.js
@@ -1,31 +1,35 @@
-import { expect, it } from "vitest";
+import { beforeEach, expect, it } from "vitest";
 import { User } from "./hooks";
 
+const testEmail = 'test@example.com';
+let user;
+
+beforeEach(() => {
+    user = new User(testEmail);
+})
 
 it('should have an email property', () => {
-    const testEmail = 'test@example.com';
-    const user = new User(testEmail);
     expect(user).toHaveProperty('email');
 })
 
-it('should update the email property', () => {
-    const testEmail = 'test@example.com';
-    const user = new User(testEmail);
+
+it.concurrent('should update the email property', () => {
     const newEmail = 'test2@example.com';
     user.updateEmail(newEmail);
     expect(user.email).toBe(newEmail);
 })
 
+it('should store the email value', () => {
+    expect(user.email).toBe(testEmail);
+})
+
+
 it('should clear the email property', () => {
-    const testEmail = 'test@example.com';
-    const user = new User(testEmail);
     user.clearEmail();
     expect(user.email).toBe('');
 })
 
 it('should clear the email property after clearing the email', () => {
-    const testEmail = 'test@example.com';
-    const user = new User(testEmail);
     user.clearEmail();
     expect(user).toHaveProperty('email')
 })
